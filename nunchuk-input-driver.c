dc8559e9b7041e4d0aafe1a52554e9400b17a23f
Sergi Granell <xerpi.g.12@gmail.com>
2015-04-10 00:09:39 +0200

    Added timer

--- nunchuk-input-driver.c
+++ nunchuk-input-driver.c
@@ -1,8 +1,21 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/input.h>
+#include <linux/timer.h>
 
 static struct input_dev *input;
+static struct timer_list timer;
+
+static void timer_function(unsigned long data)
+{
+	printk(KERN_INFO "nunchuk timer!\n");
+
+	input_report_rel(input, REL_X, 5);
+	input_report_rel(input, REL_Y, -5);
+	input_sync(input);
+
+	mod_timer(&timer, jiffies + msecs_to_jiffies(100));
+}
 
 static int __init nunchuk_input_driver_init(void)
 {
@@ -18,8 +31,15 @@ static int __init nunchuk_input_driver_init(void)
 		goto err_nomem;
 	}
 
+	input->name = "nunchuk";
+
+	set_bit(EV_REL, input->evbit);
+	set_bit(REL_X, input->relbit);
+	set_bit(REL_Y, input->relbit);
+
 	set_bit(EV_KEY, input->evbit);
-	set_bit(BTN_0, input->keybit);
+	set_bit(BTN_LEFT, input->keybit);
+	set_bit(BTN_RIGHT, input->keybit);
 	
 	error = input_register_device(input);
 	if (error) {
@@ -27,6 +47,10 @@ static int __init nunchuk_input_driver_init(void)
 			"failed to register input device (%d)\n", error);
 		goto err_freedev;
 	}
+
+	setup_timer(&timer, timer_function, 0);
+	mod_timer(&timer, jiffies + msecs_to_jiffies(100));
+	add_timer(&timer);
 	
 	return 0;
 
@@ -41,6 +65,7 @@ static void __exit nunchuk_input_driver_exit(void)
 	printk(KERN_INFO "nunchuk_input_driver_exit()\n");
 	
 	input_unregister_device(input);
+	del_timer_sync(&timer);
 }
 
 module_init(nunchuk_input_driver_init);
